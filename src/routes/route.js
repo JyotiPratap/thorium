71af1b4c9d90728139c9bb2b43777f6a79f45168
JyotiPratap <jyotiprataptiwarijee1@gmail.com>
2022-02-23 20:34:27 +0530

    Assignment23/2/2022

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,44 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('Welcome to my application')
-});
-router.get("/students/:name", function (req,res){
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send("123455")
-});
-router.get("/movies",function(req,res){
-    let array =["Rang de basanti","border","sultan","dangal"]
-    console.log(array)
-    res.send(array)
-});
-router.get("/movie/:movieId",function(req,res){
-    mov=["abs","sjdsk","sksdsk","dhsjd","ahsajh"]
-    let value = req.params.movieId;
-    if(value>mov.length-1){
-        res.send("doesnot exist")
-    }else{
-        res.send(mov[value])
-    }
-});
 
-router.get("/moviez",function(req,res){
-    res.send([{id: 1,name:"a"},{id: 2,name:"b"},{id: 3,name:"b"},{id: 4,name:"d"}])
-});
-
-router.get("films/:flims",function(req,res){
-    let movi = [{id: 1,name:"a"},{id: 2,name:"b"},{id: 3,name:"b"},{id: 4,name:"d"}]
-    let value = req.params.flimsId;
-    let found = false;
-    for(let i =0;i<movi.length;i++){
-        if(movi[i].id==value){
-            found=trueres.send(movie[i])
-            break;
-        }
-    }
-    if(found==false){
-        res.send("No movie exist with this id")}
+router.get("/sol1",function(req,res){
+    let arr= [1,2,4,5,6,7,8,9]
+        let sum =0;
+        let n = arr.length
+        for (let i =0;i<n;i++){
+            sum += arr[i]
+        }    
+        let lastdigit = arr.pop()
+      let  consecutiveSum = lastdigit*(lastdigit+1)/2
+      let missingNumber =consecutiveSum-sum
+      res.send({missingNumber: missingNumber }  );
 })
-module.exports = router;
+router.get("/sol2", function (req, res) {
+       let arr= [33, 34, 35, 37, 38]
+       let len= arr.length
+  
+      let total = 0;
+      for (var i in arr) {
+         total += arr[i];
+    }
+  
+       let firstDigit= arr[0]
+       let lastDigit= arr.pop()
+       let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+       let missingNumber= consecutiveSum - total
+   
+      res.send(  { data: missingNumber  }  );
+  });
+
