6a60465edca7b26d11f3c0b76bd83ca4786dc53b
JyotiPratap <jyotiprataptiwarijee1@gmail.com>
2022-03-01 18:37:24 +0530

    Jyoti Pratap Tiwari

--- src/controllers/bookControllers.js
+++ src/controllers/bookControllers.js
@@ -14,8 +14,13 @@ const bookList = async function (req, res) {
 const getBooksInYear = async function(req, res){
     let bookYearData = req.body.year
     let bookYear = await BookModel.find({year : bookYearData});
-    console.log('Data sent successfully');
     res.send({ "Book Data according to the year" : bookYear});
+}
+const getParticularBooks = async function(req, res) {
+    let data = req.query.any 
+    console.log(typeof (data))
+    let saveData = await BookModel.find({ $or : [{bookName : data}, {year: data}, {authorName : data}, {totalPages : data}, {stockAvailable : data}] })
+    res.send({msg : saveData})
 }
 
 // const getXINRBooks = async function(req,res){
@@ -23,8 +28,14 @@ const getBooksInYear = async function(req, res){
 //     res.send({msg:albooks})
 // }
 
+const getRandomBooks = async function(req,res){
+    let data =await BookModel.find({stockAvailable: true,totalPages:{$gte:500}})
+    res.send({msg:data})
+}
 
 module.exports.createBook= createBook
 module.exports.bookList= bookList
 module.exports.getBooksInYear= getBooksInYear
+module.exports.getParticularBooks= getParticularBooks
 // module.exports.getXINRBooks=getXINRBooks
+module.exports.getRandomBooks=getRandomBooks
